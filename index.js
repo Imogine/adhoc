16e452dd07fdf2842f544a8fdc24c0e4421ea34c
Imogine <hellogreenwhales@gmail.com>
2018-05-18 05:40:06 -0500

    initial

---
+++ index.js
@@ -0,0 +1,11 @@
+// POINTS
+
+// * Validate Data - Age and relationship (>0) MUST be validated
+// * Add People to a growing household list
+// * Remove a Previously added person from the list
+// * Display the Household in the HTML as it is modified
+// * Serialize the household as JSON upon form submissions into the 'Debug' element
+
+
+age.length < 0 
+relationship =
